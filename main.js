94cbb71ac7f8977b72149931532a7ab2139403a6
Jacob <jac21934@vt.edu>
2018-10-04 14:23:53 -0400

    Cookie Test

--- main.js
+++ main.js
@@ -241,10 +241,14 @@ function switchRightTabs(evt, tabName){
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
 
+
     }
     document.getElementById(tabName).style.display = "block";
+		
     evt.currentTarget.className += " active";
+		
 		flags[flagName] = true;
+
 
 }
 
